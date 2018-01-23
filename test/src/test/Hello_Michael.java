333bd7b56620510eae69fb0ce38a8cf7a85a8b2d
Michael Simcock <msimcock@serc.ac.uk>
2018-01-23 12:14:14 +0000

    Change-Id: I5cb3a41ad7cbb9fd13ab7b550039402da7f42ce6

--- test/src/test/Hello_Michael.java
+++ test/src/test/Hello_Michael.java
@@ -2,6 +2,6 @@ package test;
 
 public class Hello_Michael {
 	public static void main(String[] args) {
-		System.out.println("Hello Michael");
+		System.out.println("Hello Michael, how are you?");
 	}
 }
