e7e6aec2d7c13cc3dc7a8d7ba4a938f62173abfc
Michael Simcock <msimcock@serc.ac.uk>
2018-01-23 12:03:09 +0000

    Change-Id: I48868a20fbe9760cf4a1f1b6af37570dcc737d6a

--- test/src/test/Hello_World.java
+++ test/src/test/Hello_World.java
@@ -2,7 +2,7 @@ package test;
 
 public class Hello_World {
 	public static void main(String[] args) {
-		System.out.println("Hello World. Please stop, I want to get off!");
+		System.out.println("Hello World");
 	}
 
 }
